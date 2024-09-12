773543bb96e5de3899be03cab6b6a2ca0e346735
Pooja dhinchak <Pooja dhinchak@DESKTOP-441UM49>
2024-09-12 14:25:22 +0530

    Arithmetic operation

---
+++ Demo/src/p1/Hello.java
@@ -0,0 +1,15 @@
+package p1;
+
+public class Hello {
+	public static void main(String[] args) {
+		System.out.println("Arihmetic operation : ");
+		int a=20;
+		int b=30;
+		System.out.println(a+b);
+		System.out.println(b-a);
+		System.out.println(a*b);
+		System.out.println(a/b);
+		
+	}
+
+}
